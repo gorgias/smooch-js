3effebd1f40ccbed4fe3b305495d24138cb4cf7a
martinlatrille <martin@gorgias.io>
2017-10-06 11:18:57 -0700

    Remove cronjob logger

--- src/js/smooch.jsx
+++ src/js/smooch.jsx
@@ -110,7 +110,6 @@ function onStoreChange({messages, unreadCount}) {
 }
 
 function _incrementTimeSpentOnPage(store, seconds) {
-    console.log('cronjob')
     store.dispatch(AppStateActions.incrementTimeSpentOnPage(seconds));
     store.dispatch(AppStateActions.computeDisplayedCampaigns());
     setTimeout(() => _incrementTimeSpentOnPage(store, seconds), seconds * 1000);
